82576eceb4af9f61f64842502a563120a987b339
John Richard Espedal <117681128+Johnricharde@users.noreply.github.com>
2023-11-13 14:44:12 +0100

    Add project files.

---
+++ RandomHobbyGenerator/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace RandomHobbyGenerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            askForName();
+            Console.ReadKey();
+        }
+        static void askForName()
+        {
+            Console.WriteLine("Tell me your name and I'll assign you a new hobby.");
+            String userName = Console.ReadLine();
+            Console.WriteLine($"Hello {userName}!\nYou should try out {RandomHobby()}!\n");
+            askForName();
+        }
+        static string RandomHobby()
+        {
+
+            Random rand = new Random();
+            var randomNumber = rand.Next(0,3);
+
+            if (randomNumber == 0) {
+                return "knitting";
+            }
+            else if (randomNumber == 1) {
+                return "stamp collecting";
+            }
+            else
+            {
+                return "extreme rockclimbing";
+            }
+        }
+    }
+}
